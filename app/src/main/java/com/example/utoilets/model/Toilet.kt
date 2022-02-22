b9d5d0042f39cdf0d2c6c759cddad80ce66b1aa4
Anakaren Salinas <anakarensalinas01@gmail.com>
2022-02-21 21:49:50 -0600

    adding toilet model and placeholder image

---
+++ app/src/main/java/com/example/utoilets/model/Toilet.kt
@@ -0,0 +1,10 @@
+package com.example.utoilets.model
+
+import androidx.annotation.DrawableRes
+import androidx.annotation.StringRes
+
+class Toilet (@StringRes val stringResourceId: Int,
+              @StringRes val string2ResourceId: Int,
+              @DrawableRes val imageResourceId: Int
+) {
+}
